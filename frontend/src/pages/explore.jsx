b44912e2a2a14714e5b6e5e4d0e4cdd27a76c63f
Dodley <dodleydesign+github@gmail.com>
2023-05-25 23:55:23 +0100

    Fixed explore searchbar

--- frontend/src/pages/explore.jsx
+++ frontend/src/pages/explore.jsx
@@ -1,11 +1,10 @@
 import NavbarTemplate from "../templates/template_with_navbar";
-
 import NavbarProfile from "../components/navBar/navBarProfile";
 import profile from "../assets/images/Profile1.png";
 import Masonry from "../components/masonry/masonry";
 import ExploreSearchModal from "../components/modal/exploreSearchModal";
 import { toggleExploreModal } from "../store/features/uiSlice";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { CompleteLogin } from "../components/requireLogin";
 
 function Explore() {
@@ -19,13 +18,16 @@ function Explore() {
         headerText="Explore"
         pageTitle="Explore | Dive into the community to find the best pics and stories"
       >
-        <div className="space-y-4 px-1 pt-4">
-          <div
-            onClick={() => dispatch(toggleExploreModal())}
-            className="w-full lg:w-1/2 h-12 border px-4 rounded-md cursor-pointer text-gray-600 flex items-center "
-          >
-            Search users, catalogues and tags
+        <div className="relative space-y-4 px-1 pt-4">
+          <div className="py-4 bg-white sticky top-0 z-30">
+            <div
+              onClick={() => dispatch(toggleExploreModal())}
+              className="w-full px-4 lg:w-1/2 h-12 border rounded-md cursor-pointer text-gray-600 flex items-center "
+            >
+              Search users, catalogues and tags
+            </div>
           </div>
+
           <Masonry />
         </div>
       </NavbarTemplate>
@@ -38,4 +40,3 @@ export default () => (
     <Explore />
   </CompleteLogin>
 );
-
