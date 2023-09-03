fc2d1bcd4665a62c3b61fd440558cd000e9cc761
Dodley <dodleydesign+github@gmail.com>
2023-09-03 10:42:46 +0100

    Added method to copy reset link in reset_email
    - Added an extra line of text containing the reset link in the reset email, in case the email provider of the user doesn't display the original link properly

--- backend/helpers/emailReset.js
+++ backend/helpers/emailReset.js
@@ -10,30 +10,30 @@ const transporter = nodemailer.createTransport({
 
 async function sendMail(options,user) {
 
-  const HTML_MESSAGE = `
-  <div style="margin-bottom: 16px; margin-top: 16px;">
-  <h1 style="text-align: center;">Reelpic</h1>
-  <h2 style="text-align: center; margin-bottom: 4rem">Password reset</h2>
+  let HTML_MESSAGE = `<div style="display:flex;flex-direction:column;margin-bottom:16px;margin-top:16px;"><h1 style="text-align: center;">Reelpic</h1>
+    <h2 style="text-align: center;">Password reset</h2>
+    <p style="text-align: center;">
+      Someone requested that the password be reset for the following account:
+    </p>
+    <p style="text-align: center;">
+      To reset your password visit the following address
+    </p>
+    <a href="${options.text}" target="_blank" style="border-radius:.5rem;text-align:center;display:inline-block;padding:1rem 2rem;background-color:rgb(101 31 255);color:white;font-weight:800;margin:0auto;text-decoration:none;">Click to reset password
+    </a>
+    <p style="text-align: center;">Your email: <b>${user.email}</b></p>
+    <p style="text-align: center;">
+      If this was a mistake, just ignore this email and nothing will happen. Copy the text below and paste in a web browser, if the link above doesn't work. <br>
+      ${options.text}
+      <br>
+      <b>Don't share this link</b>
+    </p>
 
-  <p style="text-align: center;">
-    Someone requested that the password be reset for the following account:
-  </p>
-  <p style="text-align: center;">
-    To reset your password visit the following address
-  </p>
-    <a
-      href="${options.text}"
-      style="text-align: center;"
-      >Click to reset password</a
-    >
-  <p style="text-align: center;">Your email: <b>${user.email}</b></p>
-  <p style="text-align: center;">
-    If this was a mistake, just ignore this email and nothing will happend
-  </p>
-</div>
-  `
-  options.html = HTML_MESSAGE  
+  </div>`
+  ;
+  HTML_MESSAGE = HTML_MESSAGE.replace(/(\r\n|\n|\r)/gm," ")
+  options.html = HTML_MESSAGE    
   console.log(options)
+
   await transporter.sendMail(options, (error, info) => {
     if (error) {
       console.log(error);
