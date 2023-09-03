df4e243e97857cc19eb49f58268376d9546e2cdc
Dodley <dodleydesign+github@gmail.com>
2023-09-03 11:40:47 +0100

    Added success state to reset_done page

--- frontend/src/pages/reset_confirm/[userId]/[token].jsx
+++ frontend/src/pages/reset_confirm/[userId]/[token].jsx
@@ -5,14 +5,18 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Head from "next/head";
 import Link from "next/link";
+import ErrorMessage from "@/components/components/forms/errorMessage";
+import { IoCheckmarkDoneCircle } from "react-icons/io5";
 
 export default function () {
   const router = useRouter();
   const { token, userId } = router.query;
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
   const [allowed, setAllowed] = useState(false);
+  const [success, setSuccess] = useState(false);
   useEffect(() => {
     if (userId && token) {
-      console.log(token, userId);
       authResolver(authActions.RESET_CONFIRM, { userId, token })
         .then(() => {
           setAllowed(true);
@@ -22,6 +26,48 @@ export default function () {
         });
     }
   }, [router]);
+
+  if (success) {
+    return (
+      <>
+        <Head>
+          <title>Finished password change</title>
+        </Head>
+        <div className="relative font-sans mx-auto min-h-screen bg-[url('../assets/images/pattern_bg.webp')] flex items-center bg-cover">
+          <div className="p-2 w-full">
+            <div className=" items-center bg-white max-w-xl mx-auto my-auto rounded-xl px-[22px] sm:px-16 py-[35px] sm:py-[55px] w-full">
+              <div className="space-y-3 sm:space-y-7">
+                <div className="flex gap-3 items-center w-full justify-center">
+                  <h1 className="text-heading text-center font-logo text-logoBlue">
+                    Reelpic
+                  </h1>
+                  <div>
+                    <img
+                      src="../../logo/Logo_512x512.svg"
+                      alt="Reelpic logo"
+                      width="48"
+                      height="48"
+                    />
+                  </div>
+                </div>
+                <p className="text-center flex flex-col items-center justify-center gap-4">
+                  <IoCheckmarkDoneCircle className="w-12 h-auto text-green-600" />
+                  Password change successful
+                  <span>
+                    Now{" "}
+                    <Link className="link" href="/login">
+                      login
+                    </Link>{" "}
+                    to access your account
+                  </span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
 
   if (!(token && userId)) {
     return <LoadingScreen />;
@@ -75,18 +121,49 @@ export default function () {
                   className="w-full min-h-[280px]"
                   onSubmit={(e) => {
                     e.preventDefault();
-                    const form = e.target.elements;
-                    let password = form["password"].value;
+                    authResolver(authActions.RESET_DONE, { userId, password })
+                      .then(() => {
+                        setSuccess(true);
+                      })
+                      .catch(() => {
+                        setAllowed(false);
+                      });
                   }}
                 >
                   <div className="space-y-4 mb-4">
-                    <input
-                      placeholder="Enter password"
-                      type="password"
-                      id="password"
-                      name="password"
-                      className="h-12 w-full border px-4 rounded-md focus:outline-0 focus:ring-2 focus:ring-primary-default/50 text-dark"
-                    />
+                    <div>
+                      <label htmlFor="password">New password</label>
+                      <input
+                        onBlur={(e) => {
+                          setPassword(() => e.target.value);
+                        }}
+                        placeholder="Enter password"
+                        type="password"
+                        id="password"
+                        minLength="6"
+                        name="password"
+                        className="h-12 w-full border px-4 rounded-md focus:outline-0 focus:ring-2 focus:ring-primary-default/50 text-dark"
+                      />
+                    </div>
+                    <div>
+                      <label htmlFor="confirmPassword">Confirm password</label>
+                      <input
+                        onChange={(e) => {
+                          if (e.target.value !== password) {
+                            setMessage("Passwords don't match");
+                          } else {
+                            setMessage("");
+                          }
+                        }}
+                        placeholder="Enter password"
+                        type="password"
+                        id="confirmPassword"
+                        name="confirmPassword"
+                        minLength="6"
+                        className="h-12 w-full border px-4 rounded-md focus:outline-0 focus:ring-2 focus:ring-primary-default/50 text-dark"
+                      />
+                      <ErrorMessage message={message} />
+                    </div>
                     <button
                       type="submit"
                       //   className={`block w-full mx-auto transition duration-200 py-[14px] rounded-md text-white text-lg bg-gray-300 cursor-not-allowed"
