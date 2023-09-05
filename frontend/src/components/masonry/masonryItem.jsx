4a5a353161a324d21e95767a0fa79825eb926d67
Dodley <dodleydesign+github@gmail.com>
2023-09-06 00:10:59 +0100

    Update mansory and mansoryItem
    - Updated links in mansory items and configured it to display an icon, alongside images in case of multiple images

--- frontend/src/components/masonry/masonryItem.jsx
+++ frontend/src/components/masonry/masonryItem.jsx
@@ -1,10 +1,16 @@
+import { IoImages } from "react-icons/io5";
 import template from "../../assets/images/landscape.jpg";
 
 export default function MasonryItem({ images }) {
   if (images.length > 2) {
     return (
-      <div className=" shrink-0 last:grow">
-        <img alt="item" src={template} className="object-cover h-full" />
+      <div className=" shrink-0 last:grow relative">
+        <div className="absolute w-full inset-0 flex items-end">
+          <div className="w-full p-2  bg-dark-default/10">
+            <IoImages className="w-4 text-white h-auto" />
+          </div>
+        </div>
+        <img alt="item" src={images[0]} className="object-cover h-full" />
       </div>
     );
   }
