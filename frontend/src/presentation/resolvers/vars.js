5243b7bf91f8f643f4d7c0b20faac6c3b6eb935c
Dodley <dodleydesign+github@gmail.com>
2023-09-07 14:20:46 +0100

    Corrected vars.js

--- frontend/src/presentation/resolvers/vars.js
+++ frontend/src/presentation/resolvers/vars.js
@@ -1,2 +1,4 @@
-export const BACKEND_DOMAIN = process.env.NEXT_PUBLIC_BACKEND_DOMAIN;
+export const BACKEND_DOMAIN = process.env.NODE_ENV === 'production' ? "https://reelpicbackend.onrender.com" : "localhost:4000"
 export const AUTH_TOKEN = (token) => `Bearer ${token}`;
+
+
